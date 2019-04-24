6eda6408a28e7fd4933a8dd4a6459e07d8e98c83
boggiano <boggiano@gmail.com>
2019-04-24 17:52:45 +0200

    First based on config file

--- action-TempoIntento.py
+++ action-TempoIntento.py
@@ -1,49 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+Accende e spegne i dispositivi:
+config.ini [nostanza]
+config.ini [stanzasingoli]
+codice.py oggettiSala[]
+codice.py oggettiCamera[]
+
+La configurazione avviene con config.ini:
+[conf]
+password=
+port=8123
+ipaddress=192.168.X.X
+
+Deve leggere 4 slot dall' intento (stanza - azione - numero - oggetto)
+- Se non c'e' stanza ci riferiamo ad un oggetto generico [nostanza]
+e possiamo infliarci anche qualche nick (comodino/mensola..)
+- Se la stanza e' bagno / cucina / terrazzo (dove c'e' un solo oggetto ci riferiamo a [stanzasingoli]
+- Se la stanza e' Sala o Camera dobbiamo ragionare anche sui numeri degli oggetti
+'''
+
+
 from hermes_python.hermes import Hermes
 import configparser
 import io
 import json
 import requests 
 
+
+
+INTENT_NAME = "boggiano:AccendiSpegni"
+
+
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
-INTENT_NAME = "boggiano:AccendiSpegni"
-
 CONFIG_ENCODING_FORMAT="utf-8"
 CONFIG_INI="config.ini"
 
 
-oggettiSenzaStanza = {"caffè" : 'switch.tpcucinacaffe',
- 'modalità notte': 'input_boolean.bedtime_mode',
- 'roomba 630'    : 'switch.romba630_clean',
- 'roomba 620'    : 'switch.620_clean',
- 'sveglia relax' : 'input_boolean.svegliarelax',
- 'sveglia work'  : 'input_boolean.svegliawork'}
 
-
-oggettiSingoliStanza = {'cucina' : 'light.strip_cucina',
-'terrazzo' : 'light.luce_terrazzo',
-'bagno' : 'light.luce_bagno_1'}
-
-
+#Device in sala
 oggettiSala = ["light.luce_sala_1","light.luce_sala_2","light.luce_sala_3",
 "light.strip_sala","light.luce_sala_5",]
 
+#Device in camera
 oggettiCamera = ["light.luce_camera", "light.luce_bedlamp_2","light.luce_bedlamp"]
 
 
 
 class SnipsConfigParser(configparser.SafeConfigParser):
 
-    def to_dict(self):
-        print("Eseguo to_dict")
-        datiConfig = {section : {option_name: option for option_name, option in self.items(section)} for section in self.sections()}
-        return datiConfig
+  def to_dict(self):
+#      print("Eseguo to_dict")
+      datiConfig = {section : {option_name: option for option_name, option in self.items(section)} for section in self.sections()}
+      return datiConfig
 
 
 
@@ -65,11 +79,14 @@ def intent_received_callback(hermes, intent_message):
   # Disable warning about wrong certificates
   requests.packages.urllib3.disable_warnings()
 
-  print("Leggiamo la configurazione")
+#  print("Leggiamo la configurazione")
   conf = read_config_file(CONFIG_INI)
-  print (conf['conf']['password'])
-  print (conf['conf']['port'])
-  print (conf['conf']['ipaddress'])
+#  print (conf['conf']['password'])
+#  print (conf['conf']['port'])
+#  print (conf['conf']['ipaddress'])
+#  print (conf['nostanza']['modalità notte'])
+#  print (conf['conf']['ipaddress'])
+
 
   dominio = ""
   myDeviceId = ""
@@ -77,10 +94,10 @@ def intent_received_callback(hermes, intent_message):
   baseUrl = 'https://' + conf['conf']['ipaddress'] + ':' + conf['conf']['port'] + '/api/'
 
   header = {'Authorization': conf['conf']['password'], 'Content-Type': 'application/json'}
-  print (header)
+#  print ("[DEBUG] Header: {}".format(header))
 
 
-  print ("Intento : {}".format(intent_message.intent.intent_name))
+#  print ("Intento : {}".format(intent_message.intent.intent_name))
   sentence = ''
 
   if intent_message.intent.intent_name == INTENT_NAME:
@@ -89,6 +106,7 @@ def intent_received_callback(hermes, intent_message):
     azione    = intent_message.slots.azione.first()
     numero    = intent_message.slots.numero.first()
     oggetto   = intent_message.slots.oggetto.first()
+
 
     if stanza is not None:
      print ("[Stanza] :  -{}-".format(stanza.value))
@@ -102,84 +120,90 @@ def intent_received_callback(hermes, intent_message):
     if oggetto is not None:
       print ("[Oggetto] :  -{}-".format(oggetto.value))
 
-#    oggetto ="caffè"
 
 #Definiamo l'azione che è uguale per tutti i casi
     if (azione.value == 'accendi'):
-      print("ACCENDIAMO")
+#      print("[DEBUG] ACCENDIAMO")
       azione = 'turn_on'
     else:
-      print("SPEGNIAMO")
+#      print("[DEBUG] SPEGNIAMO")
       azione = 'turn_off'
 
 
+#CASO I
 # Se siamo senza stanza possiamo  dobbiamo giocare con gli oggetti senza stanza
 
     if (stanza is None):
-      print ("Siamo SENZA STANZA");
-      if (not oggetto.value in oggettiSenzaStanza):
-        print("ERRORE ! Non e' un oggetto contemplato")
+#      print ("Siamo SENZA STANZA");
+      if (not oggetto.value in conf['nostanza']):
+        print("ERRORE! L'oggetto -{}- non è contemplato in nel config.ini".format(oggetto.value))
       else:
 #        Se esiste dobbiamo capire qual'è il dominio dell' oggetto
-        print ("Prendiamo l'entity:")
-        print ("Entity: {}".format(oggettiSenzaStanza[oggetto.value]))
-        dominio_tmp = oggettiSenzaStanza[oggetto.value].split(".")
+
+        dominio_tmp = conf['nostanza'][oggetto.value].split(".")
         dominio = dominio_tmp[0]
-        print (dominio)
-        myDeviceId = oggettiSenzaStanza[oggetto.value]
-        print (myDeviceId)
+        del dominio_tmp
+
+#        print ("[DEBUG][NO STANZA] Dominio: -{}-".format(dominio))
+        myDeviceId = conf['nostanza'][oggetto.value]
+#        print ("[DEBUG][NO STANZA] DeviceId: -{}-".format(myDeviceId))
 
         url = baseUrl + 'services/' + dominio + '/' + azione
-        print (url)
+#        print (url)
 
 
         payload = json.dumps({"entity_id": myDeviceId})
         response = requests.post (url, headers=header, data=payload, verify=False)
 
 
-
+#CASO II
 # Abbiamo la stanza
 # Nel caso di terrazzo e bagno e cucina abbiamo solo una luce
     elif (stanza.value == 'terrazzo' or stanza.value == 'bagno' or stanza.value == 'cucina'):
 
-      print ("Siamo terrazzo o bagno o cucina")
+#      print ("Siamo terrazzo o bagno o cucina")
 
-      dominio_tmp = oggettiSingoliStanza[stanza.value].split(".")
+      dominio_tmp = conf['stanzasingoli'][stanza.value].split(".")
       dominio = dominio_tmp[0]
-      print (dominio)
+      del dominio_tmp
 
-      myDeviceId = oggettiSingoliStanza[stanza.value]
-      print (myDeviceId)
+#      print ("[DEBUG][STANZA:T/B/C] Dominio: -{}-".format(dominio))
+      myDeviceId = conf['stanzasingoli'][stanza.value]
+#      print ("[DEBUG][STANZA:T/B/C] DeviceId: -{}-".format(myDeviceId))
 
       url = baseUrl + 'services/' + dominio + '/' + azione
-      print (url)
+#      print (url)
 
       payload = json.dumps({"entity_id": myDeviceId})
       response = requests.post (url, headers=header, data=payload, verify=False)
 
-# Qui siamo nel caso di sala e camera_da_letto
+
+#Siamo nel caso di sala e camera_da_letto
 #per la sala abbiamo il problema della strip e della bedlamp
 #In camera abbiamo due bedlamp
     elif (stanza.value == 'sala'):
-      print ("Siamo in sala")
+#      print ("[DEBUG] Siamo in sala")
       dominio = 'light'
-      if numero.value is not None:
-        myDeviceId = oggettiSala[int(numero.value)-1]
-      else:
+      try:
+        if numero.value is not None:
+          myDeviceId = oggettiSala[int(numero.value)-1]
+      except:
         myDeviceId = 'group.sala'
+
 
     elif (stanza.value == 'camera'):
-      print ("Siamo in camera")
-
+#      print ("[DEBUG] Siamo in camera")
       dominio = 'light'
-      if numero is not None:
-        myDeviceId = oggettiCamera[int(numero.value)-1]
-      else:
-        myDeviceId = 'group.sala'
+      try:
+        if numero.value is not None:
+          myDeviceId = oggettiCamera[int(numero.value)-1]
+      except:
+        myDeviceId = 'group.camera'
 
-    print ("DOMINIO: {}".format(dominio))
+
+#    print ("DOMINIO: {}".format(dominio))
     url = baseUrl + 'services/' + dominio + '/' + azione
-    print (url)
+#    print (url)
 
     payload = json.dumps({"entity_id": myDeviceId})
     response = requests.post (url, headers=header, data=payload, verify=False)
@@ -191,11 +215,8 @@ def intent_received_callback(hermes, intent_message):
 #    sentence = "Va bene"
 
 
-#    hermes.publish_end_session(intent_message.session_id, sentence)
-
-
   print ("Now the voice")
-  sentence = " Benissimo"
+  sentence = "Va bene!"
   hermes.publish_end_session(intent_message.session_id, sentence)
 
   return
@@ -229,7 +250,7 @@ def intent_received_callback(hermes, intent_message):
 
 with Hermes(MQTT_ADDR) as h:
     print ("subscribe to Hermes")
-    h.subscribe_intents(intent_received_callback).start()
+    h.subscribe_intent(INTENT_NAME,intent_received_callback).start()
 
 
 
